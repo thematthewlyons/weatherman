e82c761b0fac92c74d63d7374698d9be9c0fb0fc
M Lyons <35568161+thematthewlyons@users.noreply.github.com>
2018-02-02 09:47:09 -0500

    Add files via upload

---
+++ weatherman.py
@@ -0,0 +1,26 @@
+#weatherman 0.1
+#developed by matthew lyons
+#using darksky (https://darksky.net/poweredby/) and twilio (https://www.twilio.com/docs/) APIs
+
+import forecastio
+from geopy.geocoders import Nominatim
+from twilio.rest import Client
+
+def send_weather(address):
+    api_key = "<API KEY>"   
+    location = Nominatim().geocode(address)
+    forecast = forecastio.load_forecast(api_key, location.latitude, location.longitude).currently()
+    forecast2 = forecastio.load_forecast(api_key, location.latitude, location.longitude).hourly()
+    if location: 
+        return f"Good morning! It is currently {forecast.summary}, {forecast.temperature} degrees. Today it's going to be {forecast2.summary}"
+
+def send_text():
+    account = "<TWILIO ACCOUNT NO.>"
+    token = "<TWILIO ACCT TOKEN>"
+    client = Client(account, token)
+    message = client.messages.create(to="<RECEIVING NUMBER>", from_="<TWILIO SENDING NUMBER>", body=text)
+
+text = send_weather("<CURRENT LOCATION>")
+
+send_text()
+
