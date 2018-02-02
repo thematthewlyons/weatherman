0e88944653bbe3ad57dcae796de3f3523eeead94
M Lyons <35568161+thematthewlyons@users.noreply.github.com>
2018-02-02 10:09:04 -0500

    Searchable Location
    Installed simple search function into the commandline pre-send to allow for a bit more customization.

--- weatherman.py
+++ weatherman.py
@@ -13,14 +13,17 @@ def send_weather(address):
     forecast2 = forecastio.load_forecast(api_key, location.latitude, location.longitude).hourly()
     if location: 
         return f"Good morning! It is currently {forecast.summary}, {forecast.temperature} degrees. Today it's going to be {forecast2.summary}"
-
+    else:
+        return "Location not found."
+    
 def send_text():
     account = "<TWILIO ACCOUNT NO.>"
     token = "<TWILIO ACCT TOKEN>"
     client = Client(account, token)
     message = client.messages.create(to="<RECEIVING NUMBER>", from_="<TWILIO SENDING NUMBER>", body=text)
 
-text = send_weather("<CURRENT LOCATION>")
+searchlocation = input("Where are you right now? ")
+text = send_weather(searchlocation)
 
 send_text()
 
